a2b8153b8a57496d880aef9fae9b264c2d93c49c
Bruno Rodrigues <brunosilvarodrigues2304@gmail.com>
2020-08-08 15:22:28 -0300

    Integra com backend

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -3,32 +3,52 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
-function TeacherItem(){
+export interface Teacher {
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+}
+
+interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+    function createNewConnection() {
+        api.post('connections', {
+            user_id: teacher.id,
+        })
+    }
+
     return (
         <article className="teacher-item">
             <header>
-                <img src="https://scontent.fjpa3-1.fna.fbcdn.net/v/t1.0-1/p200x200/72951536_259098058403622_1861381063071236096_n.jpg?_nc_cat=101&_nc_sid=7206a8&_nc_ohc=hD-cKbNgGTwAX85TUH9&_nc_ht=scontent.fjpa3-1.fna&_nc_tp=6&oh=8f3e057ede8b0b6176c378f953e0e266&oe=5F4DD38E" alt="Alessandra Fontes" />
+                <img src={teacher.avatar} alt={teacher.name} />
                 <div>
-                    <strong>Alessandra Fontes</strong>
-                    <span>Química</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
 
             </header>
-            <p>
-                Entusiasta das melhores tecnologias de química avançada.
-                <br /><br />
-                Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências, mais de 200 mil pessoas já passaram por uma das minhas explosões.
-                </p>
-
+            <p>{teacher.bio}</p>
             <footer>
                 <p>
-                    Preço/hora <strong>R$ 50,00</strong>
+                    Preço/hora <strong>R$ {teacher.cost}</strong>
                 </p>
-                <button type="button">
+                <a 
+                    target='_blank' 
+                    onClick={createNewConnection} 
+                    href={`https://wa.me/${teacher.whatsapp}`}
+                >
                     <img src={whatsappIcon} alt="Entrar em contato" />
-                        Entrar em contato
-                    </button>
+                    Entrar em contato
+                </a>
             </footer>
         </article>
     );
